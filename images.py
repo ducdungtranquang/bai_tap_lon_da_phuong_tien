bb4b1c5dbccfbf47c8a5399db9a0104b0a7dbaa8
danghh <danghh@coccoc.com>
2023-10-31 21:23:34 +0700

    core-v2

--- images.py
+++ images.py
@@ -8,7 +8,9 @@ def tinhLBP(image):
     anh_xam = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     ban_kinh = 1
     so_diem = 8 * ban_kinh
+
     lbp = feature.local_binary_pattern(anh_xam, so_diem, ban_kinh, method='uniform')
+
     hist, _ = np.histogram(lbp.ravel(), bins=np.arange(0, so_diem + 3), range=(0, so_diem + 2))
     hist = hist.astype("float")
     hist /= (hist.sum() + 1e-7)
@@ -21,7 +23,7 @@ def danhSachHinhAnh(folder):
         for file in files:
             if file.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp')):  # Kiểm tra các định dạng hình ảnh phổ biến
                 img_path = os.path.join(root, file)
-                docAnh = cv2.imread(img_path)
+                docAnh = cv2.imread('testSearch.png')
                 danh_sach_hinh_anh.append(docAnh)
     return danh_sach_hinh_anh
 
@@ -41,12 +43,12 @@ def tim_kiem_anh_tuong_tu(query_hist, co_so_du_lieu):
 # Bước 3: Sử dụng chức năng tìm kiếm
 def main():
     # Giả sử danh_sach_hinh_anh là danh sách các hình ảnh trong cơ sở dữ liệu của bạn
-    danh_sach_hinh_anh = danhSachHinhAnh("./images")
+    danh_sach_hinh_anh = danhSachHinhAnh("./output_images")
     # Tạo cơ sở dữ liệu histogram LBP
     co_so_du_lieu = tao_co_so_du_lieu_LBP(danh_sach_hinh_anh)
     
     # Giả sử query_img là hình ảnh bạn muốn tìm hình ảnh tương tự
-    query_img = cv2.imread('test1.png')
+    query_img = cv2.imread("test1.png")
     hist = tinhLBP(query_img)
 
     # Tìm hình ảnh tương tự
@@ -57,7 +59,7 @@ def main():
     # Lấy ra N hình ảnh tương tự hàng đầu
     so_luong_top = 5  # Thay đổi giá trị này để lấy ra số lượng hình ảnh tương tự khác nhau
     # Lấy ra chỉ số của top N hình ảnh tương tự
-    top_N_indices = indices_sap_xep[:so_luong_top].flatten()
+    top_N_indices = indices_sap_xep[1:so_luong_top].flatten()
 
     # Hiển thị hình ảnh tương tự từ danh sách đã sắp xếp
     for i, index in enumerate(top_N_indices):
@@ -66,5 +68,5 @@ def main():
         cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
